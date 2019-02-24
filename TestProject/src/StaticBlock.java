b0281daec895ef3f7715610e77d48d05906c3dcb
mail4 <mail4@DESKTOP-4T5F0SA>
2019-02-24 19:25:46 +0600

    ststic

--- TestProject/src/StaticBlock.java
+++ TestProject/src/StaticBlock.java
@@ -1,4 +1,21 @@
 
 public class StaticBlock {
+	
+	static int id;
+	static String name;
+	
+	static {
+		id = 10001;
+		name = "Atik";	
+	}
+	static void Display() {
+		System.out.println("Your Id Is:"+id);
+		System.out.println("Your Name Is:"+name);
+	}
+	
+	public static void main(String[] args) {
+		StaticBlock.Display();
+		
+	}
 
 }
