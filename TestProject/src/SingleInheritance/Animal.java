eed6eca94718caec9a553eb1eb34a1b05570382f
mail4 <mail4@DESKTOP-4T5F0SA>
2019-02-26 01:53:17 +0600

    Single inheritance

---
+++ TestProject/src/SingleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package SingleInheritance;
+
+public class Animal {
+	void eat() {
+	System.out.println("Eating");
+	}
+}
---
+++ TestProject/src/SingleInheritance/Test.java
@@ -0,0 +1,10 @@
+package SingleInheritance;
+
+public class Test {
+	public static void main(String[] args) {
+		Dog dog1 = new Dog();
+		dog1.eat();
+		dog1.bark();
+	}
+
+}
